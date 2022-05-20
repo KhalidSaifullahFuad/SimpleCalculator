8f3465ba2ba67142172c40585e146add06699880
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-05-20 23:44:55 +0600

    📦 Updated controller according to fxml

--- src/main/java/com/fuad/calculator/CalculatorController.java
+++ src/main/java/com/fuad/calculator/CalculatorController.java
@@ -3,12 +3,24 @@ package com.fuad.calculator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class CalculatorController {
 
+    private long number1;
+
+    private long number2;
+
+    private char operator;
+
     @FXML
     private Button btnAdd;
+
+    @FXML
+    private Button btnBackspace;
+
+    @FXML
+    private Button btnClear;
 
     @FXML
     private Button btnDecimal;
@@ -38,7 +50,13 @@ public class CalculatorController {
     private Button btnOne;
 
     @FXML
+    private Button btnPercent;
+
+    @FXML
     private Button btnSeven;
+
+    @FXML
+    private Button btnSign;
 
     @FXML
     private Button btnSix;
@@ -56,16 +74,24 @@ public class CalculatorController {
     private Button btnZero;
 
     @FXML
-    private TextField display;
+    private Label display;
 
     @FXML
     void doDisplay(ActionEvent event) {
-
+        String numberText = ((Button) event.getSource()).getText();
+        String displayText = display.getText();
+        display.setText(displayText.equals("0") ? numberText : display.getText() + numberText);
     }
 
     @FXML
     void doOperation(ActionEvent event) {
+        String buttonText = ((Button)event.getSource()).getText();
 
+        if(buttonText.equals("=")){
+
+        }else{
+
+        }
     }
 
 }

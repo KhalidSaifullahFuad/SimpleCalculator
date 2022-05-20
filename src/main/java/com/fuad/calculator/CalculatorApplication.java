3c2f84785685f210b6057e3af4fec643da9a66ef
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-05-20 23:44:16 +0600

    😵 Removed unnecessary code

--- src/main/java/com/fuad/calculator/CalculatorApplication.java
+++ src/main/java/com/fuad/calculator/CalculatorApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,8 @@ public class CalculatorApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("calculator-view.fxml"));
 
+        Parent root = FXMLLoader.load(getClass().getResource("calculator-view.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Calculator");
         stage.setScene(scene);

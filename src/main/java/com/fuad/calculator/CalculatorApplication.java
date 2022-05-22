b0911758cdf3d9b5e8666f93c8f2eee56da827be
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-05-22 18:05:33 +0600

    ✅ Added the font resources successfully

--- src/main/java/com/fuad/calculator/CalculatorApplication.java
+++ src/main/java/com/fuad/calculator/CalculatorApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -15,13 +17,22 @@ public class CalculatorApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         Parent root = FXMLLoader.load(getClass().getResource("calculator-view.fxml"));
+        root.getStylesheets().add(getClass().getResource("font-family.css").toExternalForm());
+
         Scene scene = new Scene(root);
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
+
+//        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+//            if(key.getCode()== KeyCode.ENTER) {
+//                System.out.println("You pressed enter");
+//            }
+//        });
     }
 
     public static void main(String[] args) {
         launch();
+
     }
 }
